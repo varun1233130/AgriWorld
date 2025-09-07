e7dd8d695ad6b3865aad5e94a65755c8d58fc0de
varun1233130 <149809355+varun1233130@users.noreply.github.com>
2025-09-07 21:45:10 +0530

    renamed test cases

--- src/test/java/com/agriworld/AgriWorldApplicationTests.java
+++ src/test/java/com/agriworld/AgriWorldApplicationTests.java
@@ -1,10 +1,10 @@
-package Config.Server.Config.Server;
+package com.agriworld;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ConfigServerApplicationTests {
+class AgriWorldApplicationTests {
 
 	@Test
 	void contextLoads() {
